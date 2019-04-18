092d108ea171e116b5a9722031f5cae6df0806ec
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-04-18 00:54:33 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -5,25 +5,29 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-  
+  name = value
+  puts name
+  return name
 end
 
-def argue
-  
+def argue(thang)
+  return thang
 end
 
-def greeting
+def greeting(greeting, name)
+  
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
-
+return "expert"
 end
 
-def pizza_party
+def pizza_party(topping = "cheese")
+  return topping
 
 end
